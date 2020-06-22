7b66622dcec6ad95cf17bf9bd82a2d692e2cf883
Hades0110 <67284455+Hades0110@users.noreply.github.com>
2020-06-23 01:17:25 +0800

    ujicoba pertama

---
+++ ujicoba.py
@@ -0,0 +1,13 @@
+"""
+SEQUENCES
+List [] - Tuples() - Dictionary{} - Sets
+
+"""
+data = {1: {'nama': 'Bagus', 'age': '17', 'hobby': 'basket'}
+        2: {'nama': 'Johan', 'umur': '21', 'hobby': 'makan'}}
+
+for key, value in data.items():
+    print("\nkeynya: ", key)
+
+    for key2 in value:
+        print(key2 + "", )
